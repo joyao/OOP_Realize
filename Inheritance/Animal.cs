76f7864d0f14359adcbc6ac131b4f94619da39fd
JYHsu <hsujoyao@gmail.com>
2020-12-27 23:59:09 +0800

    繼承

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Animal
+    {
+        protected string name = "";
+        public Animal(string name)
+        {
+            this.name = name;
+        }
+        public Animal()
+        {
+            this.name = "無名";
+        }
+
+        protected int shoutNum = 3;
+        public int ShoutNum
+        {
+            get
+            {
+                return shoutNum;
+            }
+            set
+            {
+                shoutNum = value;
+            }
+        }
+    }
+
+    class Cat : Animal
+    {
+        public Cat() : base() { }
+        public Cat(string name) : base(name) { }
+
+        public string Shout()
+        {
+            string result = "";
+            for (int i = 0; i < shoutNum; i++)
+            {
+                result += "喵，";
+            }
+            return "我的名字叫" + name + " " + result;
+        }
+    }
+
+    class Dog : Animal
+    {
+        public Dog() : base() { }
+        public Dog(string name) : base(name) { }
+
+        public string Shout()
+        {
+            string result = "";
+            for (int i = 0; i < shoutNum; i++)
+            {
+                result += "汪，";
+            }
+            return "我的名字叫" + name + " " + result;
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            cat.ShoutNum = 5;
+            Console.WriteLine(cat.Shout());
+            Console.ReadLine();
+
+            Dog dog = new Dog("狗~");
+            dog.ShoutNum = 11;
+            Console.WriteLine(dog.Shout());
+            Console.ReadLine();
+        }
+    }
+}
